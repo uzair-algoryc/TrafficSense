8eae97449762b7fb8bdbcb0dce0da5a7a43b1050
saif321761 <saifafridi32065@gmail.com>
2025-10-21 15:55:31 +0500

    runpod update

--- main.py
+++ main.py
@@ -1756,4 +1756,10 @@ print(f"Tunnel started. Urls: {tunnel.urls}")
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 80))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port, timeout_keep_alive=600)
+
+
+
+
+
+
